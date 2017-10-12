f692aee2fc876d74b062a83c0f7ac6606b72876b
Sachin Saini <sachin17897@gmail.com>
2017-10-12 11:37:56 +0530

    trying andrew's repo

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,12 @@
 var mongoose = require('mongoose');
 
-
-var User = mongoose.model('Users', {
-    email:{
-        required: true,
-        trim: true,
-        minlength: 1,
-        type: String
-    }
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-module.exports = {
-    User
-}
+module.exports = {User}
