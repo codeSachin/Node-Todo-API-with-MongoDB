e987f7b5dd74eba575d26895b9a39f6a6213a599
Sachin Saini <sachin17897@gmail.com>
2017-10-12 10:48:25 +0530

    mongo pushing to heroku yo

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,33 +7,33 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB Server');
 
-    db.collection('Todos').insertOne({
-        text: 'Sachin Saini Yo',
-        completed: false,
-        age: 20
-    }, (err, result) => {
-        if(err){
-            console.log('Could not insert the records and exiting now...');
-            return;
-        }
-        else{
-            console.log(JSON.stringify(result.ops,undefined,2));
-        }
-    });
-
-    // db.collection('Users').insertOne({
-    //     name: 'Sachin',
-    //     age: 20,
-    //     location: 'Rajpura'
-    // },(err, result)=>{
+    // db.collection('Todos').insertOne({
+    //     text: 'Sachin Saini Yo',
+    //     completed: false,
+    //     age: 20
+    // }, (err, result) => {
     //     if(err){
-    //         console.log('Could not insert a new user and now exiting...');
+    //         console.log('Could not insert the records and exiting now...');
     //         return;
     //     }
     //     else{
-    //         console.log(JSON.stringify(result.ops, undefined,2));
+    //         console.log(JSON.stringify(result.ops,undefined,2));
     //     }
     // });
+
+    db.collection('Users').insertOne({
+        name: 'Sachin',
+        age: 20,
+        location: 'Rajpura'
+    },(err, result)=>{
+        if(err){
+            console.log('Could not insert a new user and now exiting...');
+            return;
+        }
+        else{
+            console.log(JSON.stringify(result.ops, undefined,2));
+        }
+    });
 
     db.close();
 
--- server/server.js
+++ server/server.js
@@ -5,11 +5,13 @@ var {User} = require('./models/user');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use(bodyParser.json());
 app.post('/todos', (req, res)=>{
-    // console.log(req.body);
+    console.log(req.body);
     var todo = new Todo({
         text: req.body.text
     });
@@ -18,10 +20,21 @@ app.post('/todos', (req, res)=>{
         res.send(docs);
     }, (err)=>{
         res.status(400).send(e);
-    })
+    });
 });
 
-app.listen(3000, ()=> {
-    console.log('Started server on port 3000......')
+app.get('/todos', (req, res)=>{
+    Todo.find().then((doc)=>{
+        res.send({doc, code:200});
+    }, (err)=>{
+        res.send(err);
+    });
 });
 
+app.listen(port, ()=> {
+    console.log(`Started server on port ${port}......`)
+});
+
+module.exports = {
+    app
+};
